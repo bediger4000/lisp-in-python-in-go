79b430961405c769174ef81907d6497b217dce92
Bruce Ediger <bediger@stratigery.com>
2016-11-03 08:44:01 -0600

    Use passed-in environment instead of global, add lambda-defined Pr type.

--- src/lispy2/eval.go
+++ src/lispy2/eval.go
@@ -3,10 +3,10 @@ import (
 	//"fmt"
 )
 
-func Eval(node Node, env Environment) (Node){
+func Eval(node Node, env *Environment) (Node){
 	switch node.(type) {
 	case S:
-		val, found := env[node]
+		val, found := env.find(node)
 		if found {
 			return val
 		} else {
@@ -18,35 +18,41 @@ func Eval(node Node, env Environment) (Node){
 		case S:
 			switch l[0].(S) {
 			case "if":
-				cond := Eval(l[1], GlobalEnv)
+				cond := Eval(l[1], env)
 				var exp Node
 				if cond.(B) {
 					exp = l[2]
 				} else {
 					exp = l[3]
 				}
-				return Eval(exp, GlobalEnv)
+				return Eval(exp, env)
 			case "quote":
 				return l[1]
 			case "define":
-				symbol := Eval(l[1], GlobalEnv)
+				symbol := Eval(l[1], env)
 				if symbol == nil {
 					symbol = l[1]
 				}
-				value  := Eval(l[2], GlobalEnv)
-				GlobalEnv[symbol] = value
-				return nil
+				value  := Eval(l[2], env)
+				env.def(symbol, value)
+				return Node(nil)
 			case "begin":
 				var answer Node
 				for _, n := range l {
-					answer = Eval(n, GlobalEnv)
+					answer = Eval(n, env)
 				}
 				return answer
+			case "lambda":
+				pr := newProcedure(l[1], l[2], env)
+				return pr
 			}
-			proc := Eval(l[0], GlobalEnv)
+			proc := Eval(l[0], env)
 			switch proc.(type) {
 			case Fn:
-				return proc.(Fn)(l)
+				return proc.(Fn)(l, env)
+			case Pr:
+				pr := proc.(Pr)
+				return pr.run(l, env)
 			}
 		default:
 			return Node(nil)
